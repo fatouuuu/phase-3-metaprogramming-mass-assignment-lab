0d5b979d25e08ea6cfdac4d7a53e175425398385
fatouuuu <fatmaabdullrahim@gmail.com>
2023-02-18 13:37:09 +0300

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
 class Person
   # your code here
+  ATTRIBUTES = [:name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width]
+
+  attr_accessor *ATTRIBUTES
+
+  def initialize(attributes = {})
+    attributes.each do |attr, value|
+      raise ArgumentError, "unknown attribute: #{attr}" unless ATTRIBUTES.include?(attr)
+      send("#{attr}=", value)
+    end
+  end
 end
